7417c38fc69d96d6a13282fa6bb21e8edb4c80cc
“Nathan <schroedernathan2011@icloud.com>
2024-12-24 18:36:25 -0500

    Changed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const lexend = Lexend({
   variable: "--font-lexend",
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
   title: "Focus Grid",
   description:
     "Focus Grid is a productivity tool that helps you focus on your tasks and get things done.",
+  icons: {
+    icon: "/focusgridlogo.png",
+  },
 };
 
 export default function RootLayout({
